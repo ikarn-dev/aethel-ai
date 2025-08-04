4098c2abbfefa860ae84d4013c28ec0da046bd44
Karan <ikarn.dev@gmail.com>
2025-08-04 22:48:29 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.icons8.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   async headers() {
     return [
       {
